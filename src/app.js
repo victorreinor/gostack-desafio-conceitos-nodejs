5c482d2de1a5145b2290ca99440210c2a235ac05
Victor Reinor <victorreinor@gmail.com>
2020-04-15 20:24:38 -0300

    adiciona o desafio completo

---
+++ src/app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository)
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).send();
+  }
+
+  const repository = repositories[repositoryIndex];
+  repository.likes++;
+
+  return response.json(repository);
+});
+
+module.exports = app;
